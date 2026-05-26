de581c7a1e10548498e000e15164c85ce61b62f8
aimadasl523-lab <aimadasl523@gmail.com>
2026-05-26 20:13:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite' // <-- You need this line!
 
 export default defineConfig({
-  base: '/neonatal-navigator/',
+  base: '/', // <-- Change this to '/' for Vercel
   plugins: [
     tanstackStart(),
   ],
